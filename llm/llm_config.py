e3478e59bd8148347ced121d3286ccc259251bf2
Vishaal S <136082674+Vishaal-sathya@users.noreply.github.com>
2026-09-22 10:30:06 +0530

    llm config

--- llm/llm_config.py
+++ llm/llm_config.py
@@ -1,1 +1,11 @@
 # pg 117
+
+LLM_CONFIG = {
+    'vocab_size' : 50256,
+    'context_length' : 1024,
+    'emb_dim' : 768,
+    'n_heads' : 12,
+    'n_layers' : 12,
+    'drop_rate' : 0.1,
+    'qkv_bias' : False
+}
